fc2e54af67356ea63041e9a44a66f77f099233e6
Gabriel Tamura <gabriel-tamura@hotmail.com>
2018-03-14 23:05:23 -0300

    Initial check-in of module ThisAndConstructor

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ThisAndConstructor
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Soma primeiraSoma;
+
+            double numero1 = double.Parse(Console.ReadLine());
+            double numero2 = double.Parse(Console.ReadLine());
+            double numero3 = double.Parse(Console.ReadLine());
+
+            primeiraSoma = new Soma(numero1, numero2, numero3);
+
+            primeiraSoma.Resultado();
+
+            double numero4 = double.Parse(Console.ReadLine());
+            double numero5 = double.Parse(Console.ReadLine());
+            double numero6 = double.Parse(Console.ReadLine());
+
+            primeiraSoma = new Soma(numero4, numero5);
+
+            primeiraSoma.Resultado();
+
+        }
+    }
+}
---
+++ Soma.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ThisAndConstructor
+{
+    public class Soma
+    {
+        double numero1, numero2, numero3, resultado;
+
+        public Soma(double numero1, double numero2, double numero3)
+        {
+            this.numero1 = numero1;
+            this.numero2 = numero2;
+            this.numero3 = numero3;
+        }
+
+        public Soma(double numero1, double numero2)
+        {
+            this.numero1 = numero1;
+            this.numero2 = numero2;
+            this.numero3 = 0;
+        }
+
+        public void Resultado(){
+
+            resultado = numero1 + numero2 + numero3;
+
+            Console.WriteLine("O resultado eh: " + resultado);
+        }
+    }
+}
